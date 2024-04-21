9a99d02c1e6c1912fa38631eb2777e27c30cd845
hayden <hayden00721@gmail.com>
2024-04-21 23:51:34 +0900

    new file:   src/BackJoon/No10871.java

---
+++ src/BackJoon/No10871.java
@@ -0,0 +1,40 @@
+package BackJoon;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class No10871 {
+  public static void main(String[] args) throws IOException {
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+    int n = Integer.parseInt(st.nextToken());
+    int x = Integer.parseInt(st.nextToken());
+
+    int[] arr = new int[n];
+    st = new StringTokenizer(br.readLine(), " ");
+    
+
+    for(int i = 0; i < arr.length; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    for(int i = 0; i< arr.length; i++) {
+      if(arr[i]< x) {
+        bw.write(arr[i] + " ");
+      }
+    }
+
+    br.close();
+    bw.flush();
+    bw.close();
+  }
+}
